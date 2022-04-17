f00b6d9f6c95499eb2d37bb6aa37aa378504f3f6
Rajat-Arora <rarora60_be20@thapar.edu>
2022-04-17 22:22:00 +0530

    Completed intializefirstframe and findfeaturematches

--- src/stereo_vo/src/svo.cpp
+++ src/stereo_vo/src/svo.cpp
@@ -82,14 +82,91 @@ void svo::vo_callback(const sensor_msgs::ImageConstPtr& cam0_img, const sensor_m
 
 }
 
+void svo::rectify(cv::Mat& imgL, cv::Mat& imgR){
+
+	cv::Mat M1L, M2L, M1R, M2R;
+	cv::initUndistortRectifyMap(K0_, D0_, R0_, p0_, cv::Size2d(752,480), CV_32FC1, M1L, M2L);
+	cv::initUndistortRectifyMap(K1_, D1_, R1_, p1_, cv::Size2d(752,480), CV_32FC1, M1R, M2R);
+
+	cv::Mat rectified_imgL, rectified_imgR;
+
+	cv::remap(imgL, rectified_imgL, M1L, M2L, cv::INTER_LINEAR);
+	cv::remap(imgR, rectified_imgR, M1R, M2R, cv::INTER_LINEAR);
+
+	imgL = rectified_imgL;
+	imgR = rectified_imgR;
+
+	return;
+}
+
+
+void find_feature_matches(const  cv::Mat &img_1,  const  cv::Mat &img_2, 
+                          std::vector<KeyPoint> &kpL_matched, 
+                          std::vector<KeyPoint> &kpR_matched, 
+                          std::vector<DMatch> &descriptorL){
+
+	std::vector<KeyPoint> keypoints_1, keypoints_2;
+	cv::Mat descriptors_1, descriptors_2; 
+	 
+  	// -- Step 1: Detect Oriented FAST corner positions and Calculate the BRIEF descriptor based on the corner position 
+
+	svo::orb_->detectAndCompute(img_1, noArray(), keypoints1, descriptors_1);
+ 	svo::orb_->detectAndCompute(img_2, noArray(), keypoints2, descriptors_2); 
+
+	// -- Step 2: Match the Brief descriptors in the two images, using the Hamming distance 
+  	vector<DMatch> match, matches; 
+	svo::matcher_->match (descriptors_1, descriptors_2, match, Mat()); 
+
+  	// -- Step 4: Match point pair filtering 
+  	double  min_dist =  10000 , max_dist =  0 ; 
+
+  	// Find the minimum and maximum distances between all matches, that is, the distances between the most similar and least similar two sets of points 
+  	for  ( int  i =  0 ; i < descriptors_1.rows ; i++) { 
+    	double  dist = match[i].distance ; 
+    	if  (dist < min_dist) min_dist = dist; 
+    	if  (dist > max_dist) max_dist = dist; 
+  	} 
+
+
+ 	 // When the distance between descriptors is greater than twice the minimum distance, the matching is considered incorrect. But sometimes the minimum distance is very small, and an empirical value of 30 is set as the lower limit. 
+  	for  ( int  i= 0 ; i< descriptors_1.rows ; i++) { 
+    	if  (match[i].distance  <=  max ( 2  * min_dist,  30.0 )) { 
+      	matches.push_back (match[i]); 
+    	} 
+  	}
+	
+	 for(int i=0; i<matches.size();i++)
+    {
+     kpL_matched.push_back(kpts1[matches[i].queryIdx].pt);
+     kpR_matched.push_back(kpts2[matches[i].trainIdx].pt);
+     descriptorL.push_back(desc1.row(matches[i].queryIdx));
+    }
+
+	return;
+ 
+}
+
+
 
 
 void svo::initialize_first_frame(){
 
-	  const Mat& img_L = cam0_curr_img_ptr->image;
-	  const Mat& img_R = cam1_curr_img_ptr->image;
+	const Mat& img_L = cam0_curr_img_ptr->image;
+	const Mat& img_R = cam1_curr_img_ptr->image;
 	  
+	svo::rectify(img_L, img_R);
 
+	std::vector<KeyPoint> kpL_matched;
+    std::vector<KeyPoint> kpR_matched;
+    std::vector<DMatch> descriptorL;
+
+	svo::find_feature_matches(img_L, img_R, kpL_matched, kpR_matched, descriptorL);
+
+	kpL_prev_ = kpL_matched;
+	kpR_prev_ = kpR_matched;
+	desL_prev_ = descriptorL;
+	
+	return;
 }
 
 
