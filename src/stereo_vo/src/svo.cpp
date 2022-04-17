4ddbc838660e6446d5f53be8b6544dafa8dd0c57
Rajat-Arora <rarora60_be20@thapar.edu>
2022-04-17 20:21:20 +0530

    Defination for some more methods

--- src/stereo_vo/src/svo.cpp
+++ src/stereo_vo/src/svo.cpp
@@ -19,16 +19,31 @@ svo::svo(ros::NodeHandle& nodeHandle)
 }
 
 
-svo::load_parameters(){
+bool svo::load_parameters(){
 
-	
+	K0_ =  svo::getTransformCV(node_handle_, "K0");	
+	K1_ =  svo::getTransformCV(node_handle_, "K1");	
+	P0_ =  svo::getTransformCV(node_handle_, "P0");	
+	P1_ =  svo::getTransformCV(node_handle_, "P1");	
+	p0_ =  svo::getTransformCV(node_handle_, "p0");	
+	p1_ =  svo::getTransformCV(node_handle_, "p1");	
+	R0_ =  svo::getTransformCV(node_handle_, "R0");	
+	R1_ =  svo::getTransformCV(node_handle_, "R1");	
+	D0_ =  svo::getTransformCV(node_handle_, "D0");	
+	D1_ =  svo::getTransformCV(node_handle_, "D1");
 
+	node_handle_.getParam("cam0_topic", cam0_topic_);
+	node_handle_.getParam("cam1_topic", cam1_topic_);
+	node_handle_.getParam("vo_odom_topic", vo_odom_topic_);
+	node_handle_.getParam("path_topic", path_topic_);
+		
+return true;
 
 }
 
 
 
-svo::create_ros_io(){
+void svo::create_ros_io(){
 
 	cam0_img_sub_.subscribe(node_handle_, cam0_topic_ ,  10); 
   	cam1_img_sub_.subscribe(node_handle_,  cam1_topic_ ,  10); 
@@ -41,6 +56,69 @@ svo::create_ros_io(){
 
 	//  publisher for path 
 	path_pub_ = nh. advertise <nav_msgs::Path>( path_topic_ ,  10 ); 
+
+return;
 	
 }
+
+
+
+void svo::vo_callback(const sensor_msgs::ImageConstPtr& cam0_img, const sensor_msgs::ImageConstPtr& cam1_img){
+
+	cam0_curr_img_ptr = cv_bridge::toCvShare(cam0_img, sensor_msgs::image_encodings::MONO8);
+  	cam1_curr_img_ptr = cv_bridge::toCvShare(cam1_img, sensor_msgs::image_encodings::MONO8);
+
+	if (is_first_img) {
+    	
+		initializeFirstFrame();
+    	is_first_img = false;
+	}
+	else{
+	
+		
+
+	}
+
+
+}
+
+
+
+void svo::initialize_first_frame(){
+
+	  const Mat& img_L = cam0_curr_img_ptr->image;
+	  const Mat& img_R = cam1_curr_img_ptr->image;
+	  
+
+}
+
+
+
+
+
+cv::Mat  svo::getTransformCV ( const  ros::NodeHandle &nh, 
+                       const  std::string &field) { 
+  
+	cv::Mat T; 
+  	try  { 
+    //  first try reading kalibr format 
+  	T =  getKalibrStyleTransform (nh, field); 
+  	}  catch  (std::runtime_error &e) { 
+   	//  maybe it's the old style format? 
+    ROS_DEBUG_STREAM ( " cannot read transform  "  << field 
+                     <<  "  in kalibr format, trying old one! " ); 
+    try  { 
+      T =  getVec16Transform (nh, field); 
+    }  catch  (std::runtime_error &e) { 
+      std::string msg =  " cannot read transform  "  + field +  "  error:  "  + e. what (); 
+      ROS_ERROR_STREAM (msg); 
+      throw  std::runtime_error (msg); 
+    } 
+  } 
+  return  T; 
+}
+
+
+
+ 
  
