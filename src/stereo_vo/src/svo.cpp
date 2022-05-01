5096aa9f4f097b092d2c2179067431a6d7179e6e
Rajat-Arora <rarora60_be20@thapar.edu>
2022-05-01 10:09:50 +0530

    Corrected some errors

--- src/stereo_vo/src/svo.cpp
+++ src/stereo_vo/src/svo.cpp
@@ -3,24 +3,6 @@
 
 
 
-svo::svo(ros::NodeHandle& nodeHandle)
-	: node_handle_(nodeHandle)
-{
-	is_first_img_ = true;
-
-	if (!load_parameters()) {
-    	ROS_ERROR("Could not read parameters.");
-    	ros::requestShutdown();
-  	}
-
-	if (!create_ros_io()) {
-		ROS_ERROR("Could not create ROS IO.");
-        ros::requestShutdown();
-	}
-  
-}
-
-
 bool svo::load_parameters(){
 
 	K0_ =  svo::getTransformCV(node_handle_, "K0");	
@@ -43,9 +25,7 @@ return true;
 
 }
 
-
-
-void svo::create_ros_io(){
+bool svo::create_ros_io(){
 
 	cam0_img_sub_.subscribe(node_handle_, cam0_topic_ ,  10); 
   	cam1_img_sub_.subscribe(node_handle_,  cam1_topic_ ,  10); 
@@ -54,39 +34,58 @@ void svo::create_ros_io(){
 	stereo_sub_.registerCallback(&svo::vo_callback,  this);
 	
 	//  publisher for VO 
-	vo_odom_pub_ = nh. advertise <nav_msgs::Odometry>( vo_odom_topic_ ,  10 ); 
+	vo_odom_pub_ = node_handle_. advertise <nav_msgs::Odometry>( vo_odom_topic_ ,  10 ); 
+
 
 	//  publisher for path 
-	path_pub_ = nh. advertise <nav_msgs::Path>( path_topic_ ,  10 ); 
+	path_pub_ = node_handle_. advertise <nav_msgs::Path>( path_topic_ ,  10 ); 
 
-return;
+return true;
 	
+}
+
+
+svo::svo(ros::NodeHandle& nodeHandle)
+	: node_handle_(nodeHandle)
+{
+	svo::is_first_img = true;
+
+	if (!svo::load_parameters()) {
+    	ROS_ERROR("Could not read parameters.");
+    	ros::requestShutdown();
+  	}
+
+	if (!svo::create_ros_io()) {
+		ROS_ERROR("Could not create ROS IO.");
+        ros::requestShutdown();
+	}
+  
 }
 
 
 
 void svo::vo_callback(const sensor_msgs::ImageConstPtr& cam0_img, const sensor_msgs::ImageConstPtr& cam1_img){
 
-	cam0_curr_img_ptr = cv_bridge::toCvShare(cam0_img, sensor_msgs::image_encodings::MONO8);
-  	cam1_curr_img_ptr = cv_bridge::toCvShare(cam1_img, sensor_msgs::image_encodings::MONO8);
+	svo::cam0_curr_img_ptr_ = cv_bridge::toCvShare(cam0_img, sensor_msgs::image_encodings::MONO8);
+  	svo::cam1_curr_img_ptr_ = cv_bridge::toCvShare(cam1_img, sensor_msgs::image_encodings::MONO8);
 
-	if (is_first_img_) {
+	if (svo::is_first_img) {
     	
-		initializeFirstFrame();
-    	is_first_img_ = false;
+		svo::initialize_first_frame();
+    	svo::is_first_img = false;
 	}
 	else{
 		
-		const Mat& img_L = cam0_curr_img_ptr->image;
-		const Mat& img_R = cam1_curr_img_ptr->image;
+		cv::Mat img_L = cam0_curr_img_ptr_->image;
+		cv::Mat img_R = cam1_curr_img_ptr_->image;
 		
 		svo::rectify(img_L, img_R);
 
-		std::vector<KeyPoint> kpL_matched;
-    	std::vector<KeyPoint> kpR_matched;
+		std::vector<cv::KeyPoint> kpL_matched;
+    	std::vector<cv::KeyPoint> kpR_matched;
     	cv::Mat descriptorL;
-		std::vector<KeyPoint> kpL_next, kpL_prefinal, kpR_prefinal;
- 	    std::vector<KeyPoint> kpR_next, kpL_next_prefinal;
+		std::vector<cv::KeyPoint> kpL_next;
+ 	    std::vector<cv::KeyPoint> kpR_next;
 	    cv::Mat desL_next;
 
 		svo::find_feature_matches(img_L, img_R, kpL_matched, kpR_matched, descriptorL); //Find matches between previous frame.
@@ -96,14 +95,14 @@ void svo::vo_callback(const sensor_msgs::ImageConstPtr& cam0_img, const sensor_m
 		desL_next = descriptorL;
 
         //Find matches between previous and current frame.
-        std::vector<cv::Dmatch> matches_prefinal, matches_final;
-   		svo::matcher_->match (desL_prev_, desL_next, matches_prefinal, Mat()); 
+        std::vector<cv::DMatch> matches_prefinal, matches_final;
+   		svo::matcher_->match (desL_prev_, desL_next, matches_prefinal, cv::Mat()); 
         
         
   		double  min_dist =  10000 , max_dist =  0 ; 
 
   		// Find the minimum and maximum distances between all matches, that is, the distances between the most similar and least similar two sets of points 
-  		for  ( int  i =  0 ; i < matches_prefinal.rows ; i++) { 
+  		for  ( int  i =  0 ; i < matches_prefinal.size() ; i++) { 
     		double  dist = matches_prefinal[i].distance ; 
     		if  (dist < min_dist) min_dist = dist; 
     		if  (dist > max_dist) max_dist = dist; 
@@ -111,44 +110,61 @@ void svo::vo_callback(const sensor_msgs::ImageConstPtr& cam0_img, const sensor_m
 
 
  	 // When the distance between descriptors is greater than twice the minimum distance, the matching is considered incorrect. But sometimes the minimum distance is very small, and an empirical value of 30 is set as the lower limit. 
-  	for  ( int  i= 0 ; i< matches_prefinal.rows ; i++) { 
-    	if  (matches_prefinal[i].distance  <=  max ( 2  * min_dist,  30.0 )) { 
-      		matches_final.push_back (match[i]); 
+  	for  ( int  i= 0 ; i< matches_prefinal.size() ; i++) { 
+    	if  (matches_prefinal[i].distance  <=  std::max(2*min_dist, 30.0)) { 
+      		matches_final.push_back (matches_prefinal[i]); 
     	} 
   	}
 
+    std::vector<cv::Point2f> kpL_prev_prefinal, kpR_prev_prefinal, kpL_next_prefinal;
+
 	 for(int i=0; i<matches_final.size();i++)
     {
-     kpL_prefinal.push_back(kpts1[matches[i].queryIdx].pt);
-     kpR_prefinal.push_back(kpts2[matches[i].trainIdx].pt);
+     kpL_prev_prefinal.push_back(kpL_prev_[matches_final[i].queryIdx].pt);
+     kpR_prev_prefinal.push_back(kpR_prev_[matches_final[i].queryIdx].pt);
+     kpL_next_prefinal.push_back(kpL_next[matches_final[i].queryIdx].pt);
     }
 
-	return;
+    // Set previous to current frame for next iteration
+    kpL_prev_ =  kpL_next;
+    kpR_prev_ =  kpR_next;
+    desL_prev_ = desL_next;
+
+
+    cv::Mat A,A1,rvec,tvec,_R,k1,k2,k3,pos;
+    cv::triangulatePoints(P0_,P1_,kpL_prev_prefinal,kpR_prev_prefinal,A);
+    cv::convertPointsFromHomogeneous(A.t(),A1);
+
+    cv::decomposeProjectionMatrix(P0_,k1,k2,k3);
+    cv::solvePnPRansac(A1,kpL_next_prefinal,k1,cv::noArray(),rvec,tvec,false,145,1.0,0.99,cv::noArray(),cv::SOLVEPNP_ITERATIVE);
  
+    Rodrigues(rvec,_R);
+    voR = _R * voR;
+    voT = _R * voT + tvec;
+    pos_n = -voR.t() * voT; //translation of current w.r.t initial
+
+
+  //  cv::Mat rot; // rvec_initial_frame;
+  //  rot = voR.t(); //rotation matrix w.r.t initial
+
+ //   Matrix3f rot_eigen;
+  //  cv2eigen(rot,rot_eigen);
+  //  Vector3f rvec_initial_frame = rot_eigen.eulerAngles(2,1,0);
+  //  pos_n = pos; //current w.r.t initial imu frame
+ // T_vo_w.setOrigin(tf::Vector3(pos_n.at<double>(0,0),pos_n.at<double>(1,0),pos_n.at<double>(2,0)));
+//  quat_rvec.setRPY(rvec_initial_frame[0],rvec_initial_frame[1],rvec_initial_frame[2]);
+//  quat_rvec.normalize();
+//  T_vo_w.setRotation(quat_rvec);
+
+
+
+}
+
+	return;
 }
 
 
-
-
-
-
-		svo::filter_matches(kpL_prev_, kpR_prev_, )
-
-	}
-
-
-}
-
-void svo::filter_matches(){
-	
-	
-
-
-
-}
-
-
-void svo::rectify(cv::Mat& imgL, cv::Mat& imgR){
+void svo::rectify(cv::Mat imgL, cv::Mat imgR){
 
 	cv::Mat M1L, M2L, M1R, M2R;
 	cv::initUndistortRectifyMap(K0_, D0_, R0_, p0_, cv::Size2d(752,480), CV_32FC1, M1L, M2L);
@@ -166,22 +182,22 @@ void svo::rectify(cv::Mat& imgL, cv::Mat& imgR){
 }
 
 
-void find_feature_matches(const  cv::Mat &img_1,  const  cv::Mat &img_2, 
-                          std::vector<KeyPoint> &kpL_matched, 
-                          std::vector<KeyPoint> &kpR_matched, 
-                          std::vector<DMatch> &descriptorL){
+void svo::find_feature_matches(const cv::Mat& img_1, const cv::Mat& img_2, 
+                          std::vector<cv::KeyPoint>& kpL_matched, 
+                          std::vector<cv::KeyPoint>& kpR_matched, 
+                          cv::Mat& descriptorL){
 
-	std::vector<KeyPoint> keypoints_1, keypoints_2;
+	std::vector<cv::KeyPoint> keypoints_1, keypoints_2;
 	cv::Mat descriptors_1, descriptors_2; 
 	 
   	// -- Step 1: Detect Oriented FAST corner positions and Calculate the BRIEF descriptor based on the corner position 
 
-	svo::orb_->detectAndCompute(img_1, noArray(), keypoints1, descriptors_1);
- 	svo::orb_->detectAndCompute(img_2, noArray(), keypoints2, descriptors_2); 
+	svo::orb_->detectAndCompute(img_1, cv::noArray(), keypoints_1, descriptors_1);
+ 	svo::orb_->detectAndCompute(img_2, cv::noArray(), keypoints_2, descriptors_2); 
 
 	// -- Step 2: Match the Brief descriptors in the two images, using the Hamming distance 
-  	vector<DMatch> match, matches; 
-	svo::matcher_->match (descriptors_1, descriptors_2, match, Mat()); 
+  	std::vector<cv::DMatch> match, matches; 
+	svo::matcher_->match (descriptors_1, descriptors_2, match, cv::Mat()); 
 
   	// -- Step 4: Match point pair filtering 
   	double  min_dist =  10000 , max_dist =  0 ; 
@@ -196,16 +212,16 @@ void find_feature_matches(const  cv::Mat &img_1,  const  cv::Mat &img_2,
 
  	 // When the distance between descriptors is greater than twice the minimum distance, the matching is considered incorrect. But sometimes the minimum distance is very small, and an empirical value of 30 is set as the lower limit. 
   	for  ( int  i= 0 ; i< descriptors_1.rows ; i++) { 
-    	if  (match[i].distance  <=  max ( 2  * min_dist,  30.0 )) { 
+    	if  (match[i].distance  <=  std::max(2*min_dist, 30.0)) { 
       	matches.push_back (match[i]); 
     	} 
   	}
 
 	 for(int i=0; i<matches.size();i++)
     {
-     kpL_matched.push_back(kpts1[matches[i].queryIdx].pt);
-     kpR_matched.push_back(kpts2[matches[i].trainIdx].pt);
-     descriptorL.push_back(desc1.row(matches[i].queryIdx));
+     kpL_matched.push_back(keypoints_1[matches[i].queryIdx].pt);
+     kpR_matched.push_back(keypoints_2[matches[i].trainIdx].pt);
+     descriptorL.push_back(descriptors_1.row(matches[i].queryIdx));
     }
 
 	return;
@@ -217,14 +233,14 @@ void find_feature_matches(const  cv::Mat &img_1,  const  cv::Mat &img_2,
 
 void svo::initialize_first_frame(){
 
-	const Mat& img_L = cam0_curr_img_ptr->image;
-	const Mat& img_R = cam1_curr_img_ptr->image;
+	 cv::Mat img_L = cam0_curr_img_ptr_->image;
+	 cv::Mat img_R = cam1_curr_img_ptr_->image;
 	  
 	svo::rectify(img_L, img_R);
 
-	std::vector<KeyPoint> kpL_matched;
-    std::vector<KeyPoint> kpR_matched;
-    std::vector<DMatch> descriptorL;
+	std::vector<cv::KeyPoint> kpL_matched;
+    std::vector<cv::KeyPoint> kpR_matched;
+    cv::Mat descriptorL;
 
 	svo::find_feature_matches(img_L, img_R, kpL_matched, kpR_matched, descriptorL);
 
